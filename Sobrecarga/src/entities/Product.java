5729aecbfa3a19df277304958b400fbf6b127ebb
Leonardo Santos <leonardojosantos@gmail.com>
2023-11-19 18:19:24 -0300

    overload

---
+++ Sobrecarga/src/entities/Product.java
@@ -0,0 +1,48 @@
+package entities;
+
+public class Product {
+	/*
+	 * attribute
+	 */
+	public String name;
+	public double price;
+	public int quantity;
+
+	/*
+	 * constructor
+	 */
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	/*
+	 * constructor - overload
+	 */
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+		this.quantity = 0;
+	}
+
+	/*
+	 * methods
+	 */
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+
+	public String toString() {
+		return String.format("%s, ", this.name) + String.format("$ %.2f, ", this.price)
+				+ String.format("%d unidades, ", this.quantity) + String.format("$ %.2f%n", this.totalValueInStock());
+	}
+}
