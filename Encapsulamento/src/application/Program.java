1ed971432bd6a62afa064900bb58f94f67006165
Leonardo Santos <leonardojosantos@gmail.com>
2023-11-19 18:39:01 -0300

    encapsulation

---
+++ Encapsulamento/src/application/Program.java
@@ -0,0 +1,84 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+
+		Scanner scanner = new Scanner(System.in);
+
+		Product product;
+		String name = "";
+		double price = 0.0;
+
+		System.out.println("\nEntre com os dados do produto");
+
+		name = requestName(scanner);
+		price = requestPrice(scanner);
+
+		product = new Product(name, price);
+
+		product.setName("Computador");
+
+		System.out.printf("\nAtualizado nome do produto: %s%n", product.getName());
+
+		product.setPrice(1200.00);
+
+		System.out.printf("\nAtualizado preço do produto: %s%n", product.getPrice());
+
+		displayData(product);
+
+		requestQuantityOfProductToAddInStock(scanner, product);
+
+		displayData(product);
+
+		requestQuantityOfProductToRemoveFromStock(scanner, product);
+
+		displayData(product);
+
+		scanner.close();
+
+		System.out.println("-> fim do programa");
+
+	}
+
+	/*
+	 * functions
+	 */
+	private static String requestName(Scanner scanner) {
+		String name = "";
+		System.out.print("Nome do produto: ");
+		name = scanner.nextLine().toUpperCase().trim();
+		return name;
+	}
+
+	private static double requestPrice(Scanner scanner) {
+		double price = 0.0;
+		System.out.print("Preço do produto: ");
+		price = scanner.nextDouble();
+		return price;
+	}
+
+	private static void displayData(Product product) {
+		System.out.println("\nExibindo dados do Produto");
+		System.out.println(product.toString());
+	}
+
+	private static void requestQuantityOfProductToAddInStock(Scanner scanner, Product product) {
+		System.out.print("Entre com a quantidade de produtos a ser adicionado no estoque: ");
+		int quantityToBeAdded = scanner.nextInt();
+		product.addProducts(quantityToBeAdded);
+	}
+
+	private static void requestQuantityOfProductToRemoveFromStock(Scanner scanner, Product product) {
+		System.out.print("Entre com a quantidade de produtos a ser removido no estoque: ");
+		int quantityToBeRemoved = scanner.nextInt();
+		product.removeProducts(quantityToBeRemoved);
+	}
+}
---
+++ Encapsulamento/src/entities/Product.java
@@ -0,0 +1,67 @@
+package entities;
+
+public class Product {
+	/*
+	 * attribute
+	 */
+	private String name;
+	private double price;
+	private int quantity;
+
+	/*
+	 * constructor
+	 */
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	/*
+	 * constructor - overload
+	 */
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+		this.quantity = 0;
+	}
+
+	/*
+	 * getters and setters
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	/*
+	 * methods
+	 */
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+
+	public String toString() {
+		return String.format("%s, ", this.name) + String.format("$ %.2f, ", this.price)
+				+ String.format("%d unidades, ", this.quantity) + String.format("$ %.2f%n", this.totalValueInStock());
+	}
+}
